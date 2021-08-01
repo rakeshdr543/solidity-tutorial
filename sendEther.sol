a077014ff2f1f20cd9233a6f588d7e2a74caa8fc
Rakesh D R <rakeshdr543@gmail.com>
2021-08-01 19:14:18 +0530

    Added send ether

---
+++ sendEther.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.5.1;
+
+contract MyContract {
+    mapping(address => uint256) public balances;
+    address payable wallet;
+
+    constructor(address payable _wallet) public {
+        wallet = _wallet;
+    }
+
+    function buyToken() public payable {
+        //buy a buyToken
+        balances[msg.sender] += 1;
+        //send ether to the wallet
+        wallet.transfer(msg.value);
+    }
+}
